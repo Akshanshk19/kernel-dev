6ef96faf4bb116f8b59807bca4ec01857defa2b2
Akshanshk19 <akshanshk9169@gmail.com>
2026-04-08 20:04:06 +0530

    refactor: reorganized the repo

---
+++ 03_gpio/gpio.mod.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x60634223, "gpiod_set_value" },
+	{ 0x31da6023, "gpio_to_desc" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xb986a570, "gpiod_direction_output" },
+	{ 0x68cc1b47, "gpiod_get_value" },
+	{ 0x474e54d2, "module_layout" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "8F72A2B7F547245E387EFC5");
